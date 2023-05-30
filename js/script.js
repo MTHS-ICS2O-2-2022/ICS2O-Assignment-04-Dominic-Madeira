909b4ab490b58bcf99abe50edee7247175ef4a1e
Dominic-Madeira <124389519+Dominic-Madeira@users.noreply.github.com>
2023-05-30 16:13:36 +0000

    fixed bugs

--- js/script.js
+++ js/script.js
@@ -13,36 +13,29 @@
 
 function myButtonClicked() {
   // input
+  const TAX = 0.13
   const subMeat = parseInt(document.getElementById('sub-meat').value)
   const subLength = parseInt(document.getElementById('sub-length').value)
-  
+  let cost = 0
 
   // process
     if (subLength == 6) {
     if (subMeat == 1) {
-      const cost = 7.00
-      const TAX = 0.13
-      const price = TAX * cost + cost
+      cost = 7.00
       document.getElementById('answer').innerHTML = 'Your total comes to $' + price.toFixed(2) + '. Thank you for eating at Subway!'
     } else if (subMeat == 2) {
-      const cost = 5.00
-      const TAX = 0.13
-      const price = TAX * cost + cost
+      cost = 5.00
       document.getElementById('answer').innerHTML = 'Your total comes to $' + price.toFixed(2) + '. Thank you for eating at Subway!'
     } else if (subMeat == 3) {
-      const cost = 5.50
-      const TAX = 0.13
-      const price = TAX * cost + cost
+      cost = 5.50
       document.getElementById('answer').innerHTML = 'Your total comes to $' + price.toFixed(2) + '. Thank you for eating at Subway!'
     } else if (subMeat == 4) {
-      const cost = 6.00
-      const TAX = 0.13
-      const price = TAX * cost + cost
+      cost = 6.00
       document.getElementById('answer').innerHTML = 'Your total comes to $' + price.toFixed(2) + '. Thank you for eating at Subway!'
     } else {
       document.getElementById('answer').innerHTML = 'Please fill out the form completely.'
     }
-  
+    let price = TAX * cost + cost
   } else  if (subLength == 12) {
 
     if (subMeat == 1) {
